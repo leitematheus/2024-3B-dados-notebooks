92a60fb5826aed48f316b26a8523730a71402d29
Matheus Henrique Leite da Silva <110172122+leitematheus@users.noreply.github.com>
2024-10-31 10:42:49 -0300

    31/10

--- graficos/dadosGerais.js
+++ graficos/dadosGerais.js
@@ -1,18 +1,13 @@
-const url = 'https://raw.githubusercontent.com/leitematheus/API-3B-notebooks/refs/heads/main/Marcas_notebooks.json'
+const url = 'https://raw.githubusercontent.com/leitematheus/API-3B-notebooks/refs/heads/main/not_norte.json'
 
 
 async function visualizarInformacoesGerais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const totalEntrevistados = (dados.numero_de_brasileiros / 1e6)
-    console.log(dados.notebooks_por_regiao.sul[0])
+    const notebooks = Object.keys(dados)
+    const notebookMaisVotados = notebooks[0]
+    const quantidadeDeVoto = Object.values(dados)[0]
 
-
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('caixa-grafico_texto')
-    paragrafo.innerHTML = `Entrevistamos <span>${totalEntrevistados} milhão</span> de brasileiros para descobrir quais são os notebooks favoritos por todos`
-
-    const container = document.getElementById('caixa-grafico')
-    container.appendChild(paragrafo)
 }
+
 visualizarInformacoesGerais()
